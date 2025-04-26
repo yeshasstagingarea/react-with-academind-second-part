55bc310784b93460b0f4e25490a5c4f49c2f15d7
malkesh.m <malkesh.m@gmail.com>
2025-04-26 09:39:42 +0530

    Outputtng lists

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,27 @@
+const DUMMY_DATA = [
+    {
+        id:'m1',
+        title:'This is a first meetup',
+        image: 'https://assets.vogue.com/photos/58efe7b395a2fe5824075380/master/w_1600%2Cc_limit/02-new-cornelia-street-apartment.jpg',
+        address: 'Cornelia street',
+        description:'First meetup'
+    },
+    {
+        id:'m2',
+        title:'This is a second meetup',
+        image: 'https://hips.hearstapps.com/hmg-prod/images/taylor-swift-rhode-island-house-1595612001.jpg?crop=0.563xw:1.00xh;0.224xw,0&resize=640:*',
+        address: 'Holiday house',
+        description:'second meetup'
+    }
+];
+
 function AllMeetupPage(){
-    return(<div>All meetup page</div>);
+    return(<section>
+        <h1>All Meetups Page</h1>
+        <ul>
+            {DUMMY_DATA.map((meetup)=><li key={meetup.id}>{meetup.title}</li>)}
+        </ul>
+        </section>);
 }
 
 
